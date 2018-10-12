54113463c523b11fef54504d12c4a08e5fe2a9e1
Stefano Cudini <stefano.cudini@gmail.com>
2018-10-12 17:15:24 +0200

    refatoring, tests server

---
+++ bin/cli.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+var fetchdom = require('../lib/node-fetch-dom');
+
+var url = process.argv[2] || undefined,
+    subvar = process.argv[3] || undefined;
+
+fetchdom(url, subvar, function(dom) {
+	
+	var json = JSON.stringify(dom, null, 4);
+
+	console.log(json);
+
+});
---
+++ tests/server.js
@@ -0,0 +1,26 @@
+
+var restify = require('restify');
+var fetchdom = require('../lib/node-fetch-dom');
+
+
+var server = restify.createServer();
+
+server.use(restify.plugins.queryParser());
+
+server.get('/', function(req, res, next) {
+
+	console.log('fetchdom', req.query);
+
+	fetchdom(req.query.url, req.query.dom, function(dom) {
+
+		var json = JSON.stringify(dom, null, 4);
+		
+		res.send(dom);
+
+		return next();
+	});
+});
+
+server.listen(3000, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
