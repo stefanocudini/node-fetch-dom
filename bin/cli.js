d3648b5e31a1837cb77f4669d61bc30b435aaee5
Stefano Cudini <stefano.cudini@gmail.com>
2018-10-26 11:28:09 +0200

    add wait option

--- bin/cli.js
+++ bin/cli.js
@@ -3,9 +3,12 @@
 var fetchdom = require('../lib/fetch-dom');
 
 var url = process.argv[2] || undefined,
-    subvar = process.argv[3] || undefined;
+	opts = {
+		subvar: process.argv[3] || undefined,
+		wait: process.argv[4] || undefined,
+	};
 
-fetchdom(url, subvar, function(dom) {
+fetchdom(url, opts, function(dom) {
 	
 	var json = JSON.stringify(dom, null, 4);
 
