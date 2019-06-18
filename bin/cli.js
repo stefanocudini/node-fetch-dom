9d44fa90915ae8e4abc5724e382c817ac456701a
stefanocudini <stefano.cudini@gmail.com>
2019-06-18 10:35:01 +0200

    help

--- bin/cli.js
+++ bin/cli.js
@@ -2,6 +2,11 @@
 
 var fetchdom = require('../lib/fetch-dom');
 
+if(process.argv.length<3) {
+	process.stderr.write("pass url and var name\n");
+	return;
+}
+
 var url = process.argv[2] || undefined,
 	opts = {
 		subvar: process.argv[3] || undefined,
@@ -12,6 +17,6 @@ fetchdom(url, opts, function(dom) {
 	
 	var json = JSON.stringify(dom, null, 4);
 
-	console.log(json);
+	process.stdout.write(json)
 
 });
